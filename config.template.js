0e86ef08cb18b568fd2913aba85406e7114befa0
OSL <oslslapshot@gmail.com>
2023-06-28 18:43:07 +0800

    Complete revamp of queue system, including a new class for the queue with a lot more automation for different modes with some elements still hardcoded for displaying appropriately in embeds, added button and menu interactions for queue-related actions, Dropped support for the typed queue and match report commands (admin report is still typed), Queue status embed now shows a clock symbol for those who have been alerted for idling until they are kicked with no alerts now displayed for when a player is kicked, Updated the support command embed, Updated info command embed, Changed the slapshotQueue.js file to the LivePlayerTracker() class, and added an about button instead of the page link in the footer, Added modes to the config file so that the addition of new modes is easier, Significantly improved leaderboard command to include sorting (for rating, matches, wins, losses in ascending/descending order) via buttons, retired leaderboard commands specific to each mode since each mode can be accessed via buttons with the normal leaderboard command, changed sortByRatings to sortBy to support the improved leaderboard command, echo and hiDad function bug fix, Added a button state refresh for draft check-in five seconds of idle time after last interaction, Fixed bugs in draftClass.js

--- config.template.js
+++ config.template.js
@@ -11,24 +11,65 @@ const cfg = {
         production: ''	//server name
     },
     cmdChannels: {
-        development: ['test'],	//not used
-		production: {	//All the listed channels are required to be created and visible to the bot on the server or else the bot will throw an error on startup
-			casualCh: 'rpugs-casual',
-			twosCh: 'rpugs-twos',
-			foursCh: 'rpugs-fours',
-			scrimsCh: 'rpugs-scrims',
-			infoCh: 'rpugs-rules',
+        development: ['test'],
+		production: {
+			liveCh: 'rebound-oce-live',
 			modCh: 'rpugs-moderation',
-			otherCh: 'misc',
+			infoCh: 'rpugs-rules',
 			updatesCh: 'rpugs-status',
+			queueCh: 'rpugs-queues',
+			commandsCh: 'rpugs-commands',
 			tipsCh: 'general',
+			otherCh: 'misc',
 		}
     },
-	idleParams: {	//Queue idle checking
-		idleCheckerEnable: 1,	//0 or 1
-		idleThresholdmin: 5,	//0.0025 ; // Threshold for when to alert players they will be kicked for idling in units of minutes
+	queueStatusMsgId: null,	//Set to null to have the bot send a new message on start up- console will print the message ID, set to the message ID to edit the message in cmdChannels.queueCh on startup instead of sending a new message
+
+	slapshotLiveTracker: {
+		//channelId: '',	//unused- using cmdChannels.liveCh instead
+		enable: true,
+		msgId: null,	//Set to null to have the bot send a new message on start up- console will print the message ID, set to the message ID to edit the message in cmdChannels.queueCh on startup instead of sending a new message
+		refreshTime: 6	//seconds
+	},
+	
+	modes: [
+		{
+			modeName: 'Casual',
+			designator: 'c',
+			emoji: '3️⃣',
+			//draftOrder: getDraftOrder(mode),
+			numPlayers: 6,
+			arenaName: 'Slapstadium'	//change to arenaNames and array in future
+		},
+		{
+			modeName: 'Twos',
+			designator: 't',
+			emoji: '2️⃣',
+			//draftOrder: getDraftOrder(mode),
+			numPlayers: 4,
+			arenaName: 'Slapstadium_Mini'
+		},
+		{
+			modeName: 'Fours',
+			designator: 'f',
+			emoji: '4️⃣',
+			//draftOrder: getDraftOrder(mode),
+			numPlayers: 8,
+			arenaName: 'Slapville_Jumbo'
+		},
+		{
+			modeName: 'Scrims',
+			designator: 's',
+			emoji: '<:osl:724558511891021824>',
+			numPlayers: 2,
+			arenaName: 'Slapstadium'
+		}
+	],
+	idleParams: {
+		idleCheckerEnable: true,	//enable/disable idle check for queues
+		idleThresholdmin: 30,	//0.0025 ; // Threshold for when to alert players they will be kicked for idling in units of minutes
 		kickThresholdmin: 5,	//0.1 ; // Threshold for when to kick players for idling after they have been alerted in units of minutes
-		checkPeriodmin: 0.01	//0.01 ; // Period for calling idle check function in units of minutes
+		checkPeriodmin: 5	//0.01 ; // Period for calling idle check function in units of minutes
 	},
     trueskill: {	//starting ratings
         casualInitTS: {
